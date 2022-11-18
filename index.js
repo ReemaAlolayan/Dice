e821ab03c17d556e856da7edb5c0301a3abdf63e
ReemaAlolayan <109808021+ReemaAlolayan@users.noreply.github.com>
2022-11-18 20:47:03 +0300

    Add Files

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+
+
+var randomNumber1 = Math.floor(Math.random()*6) + 1 ;
+var randomImg1 = "images/dice" + randomNumber1 +".png";
+document.querySelectorAll("img")[0].setAttribute("src" , randomImg1);
+
+
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1 ;
+var randomImg2 = "images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src" , randomImg2);
+
+// img1
+
+
+if(randomNumber1 > randomNumber2){
+document.querySelector("h1").innerHTML = "Player1 wins!";
+}else if(randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML="Player2 wins!";
+}else{
+  document.querySelector("h1").innerHTML="Draw!";
+
+}
